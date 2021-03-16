6e940c732c52c66465c234f2e3a2e3497c13663e
Alikuxac <lexson270400@gmail.com>
2021-03-16 10:05:19 +0700

    Add project files.

---
+++ Buoi4_Phan1CRUD/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Buoi4_Phan1CRUD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
